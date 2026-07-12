1d4bb889f28ddc737ed2c75d82898f57547cf284
Tony Manino <unfool@unfool.com>
2026-07-12 10:33:16 -0400

    Updates to indicator shared placeholder text

--- mq-blind-spots-indicator-shared-placeholder.rb
+++ mq-blind-spots-indicator-shared-placeholder.rb
@@ -1,21 +1,14 @@
-############################################################################################
+#########################################################################################
 #
 #  mq_blind_spots indicator
 #
-#  To update the levels of this indicator, replace this entire script as follows:
-#
-#  1. Navigate to the *Indices & Stocks > End Of Day* section of the MenthorQ web site
-#  2. Make sure you have the *Blind Spots Levels* block (Command) on the page
-#  3. Click *The Levelator* bookmarklet to capture the blind spot levels to the clipboard
-#  4. Click within this script so you see a cursor
-#  5. Press CTRL-a to select ALL of the script
-#  6. Paste the clipboard contents by pressing CTRL-v
-#  7. Click OK to close the indicator script
-#  8. Click OK to close the studies window
-#
-#  (Yes, replace this text! It's just a placeholder to get you started.)
+#  To update the levels of this indicator, replace the entire text of this script with
+#  the blind spot clipboard output of The Levelator bookmarklet.
+#  
+#  (Yes, replace all of this text! It's just a placeholder to get you started with
+#  the shared the indicator.)
 #
 #  For more details, see https://unfool.github.io/tos-mq/
 #
-############################################################################################
+#########################################################################################
 plot DoNothing = Double.NaN;
--- mq-gex-indicator-shared-placeholder.rb
+++ mq-gex-indicator-shared-placeholder.rb
@@ -1,21 +1,14 @@
-############################################################################################
+#########################################################################################
 #
 #  mq_gex indicator
 #
-#  To update the levels of this indicator, replace this entire script as follows:
-#
-#  1. Navigate to the *Indices & Stocks > Intraday* section of the MenthorQ web site
-#  2. Make sure you have the *TradingView Levels Intraday* block (Command) on the page
-#  3. Click *The Levelator* bookmarklet to capture the GEX levels to the clipboard
-#  4. Click within this script so you see a cursor
-#  5. Press CTRL-a to select ALL of the script
-#  6. With the text selected, paste the clipboard contents by pressing CTRL-v
-#  7. Click OK to close the indicator script
-#  8. Click OK to close the studies window
-#
-#  (Yes, replace this text! It's just a placeholder to get you started.)
+#  To update the levels of this indicator, replace the entire text of this script with
+#  the GEX clipboard output of The Levelator bookmarklet.
+#  
+#  (Yes, replace all of this text! It's just a placeholder to get you started with
+#  the shared the indicator.)
 #
 #  For more details, see https://unfool.github.io/tos-mq/
 #
-############################################################################################
+#########################################################################################
 plot DoNothing = Double.NaN;

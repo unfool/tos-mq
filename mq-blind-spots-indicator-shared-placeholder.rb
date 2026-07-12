7cecdec5b5928949391452b94460858c83400e06
Tony Manino <unfool@unfool.com>
2026-07-12 10:20:29 -0400

    Add indicator shared placeholders

---
+++ mq-blind-spots-indicator-shared-placeholder.rb
@@ -0,0 +1,21 @@
+############################################################################################
+#
+#  mq_blind_spots indicator
+#
+#  To update the levels of this indicator, replace this entire script as follows:
+#
+#  1. Navigate to the *Indices & Stocks > End Of Day* section of the MenthorQ web site
+#  2. Make sure you have the *Blind Spots Levels* block (Command) on the page
+#  3. Click *The Levelator* bookmarklet to capture the blind spot levels to the clipboard
+#  4. Click within this script so you see a cursor
+#  5. Press CTRL-a to select ALL of the script
+#  6. Paste the clipboard contents by pressing CTRL-v
+#  7. Click OK to close the indicator script
+#  8. Click OK to close the studies window
+#
+#  (Yes, replace this text! It's just a placeholder to get you started.)
+#
+#  For more details, see https://unfool.github.io/tos-mq/
+#
+############################################################################################
+plot DoNothing = Double.NaN;
---
+++ mq-gex-indicator-shared-placeholder.rb
@@ -0,0 +1,21 @@
+############################################################################################
+#
+#  mq_gex indicator
+#
+#  To update the levels of this indicator, replace this entire script as follows:
+#
+#  1. Navigate to the *Indices & Stocks > Intraday* section of the MenthorQ web site
+#  2. Make sure you have the *TradingView Levels Intraday* block (Command) on the page
+#  3. Click *The Levelator* bookmarklet to capture the GEX levels to the clipboard
+#  4. Click within this script so you see a cursor
+#  5. Press CTRL-a to select ALL of the script
+#  6. With the text selected, paste the clipboard contents by pressing CTRL-v
+#  7. Click OK to close the indicator script
+#  8. Click OK to close the studies window
+#
+#  (Yes, replace this text! It's just a placeholder to get you started.)
+#
+#  For more details, see https://unfool.github.io/tos-mq/
+#
+############################################################################################
+plot DoNothing = Double.NaN;
